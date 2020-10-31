ef92123c92ef26c3c360e041a3d4d18cf10fb98a
Carlos Torrealba <calrrox@gmail.com>
2020-10-31 06:52:01 -0400

    Add right method

--- lib/space_school.rb
+++ lib/space_school.rb
@@ -1,9 +1,10 @@
 require_relative "space_school/version"
 require_relative 'space_school/game'
+require_relative 'space_school/ship'
 
 module SpaceSchool
   class Error < StandardError; end
   # Your code goes here...
 end
 
-Game.start
+# Game.start
--- lib/space_school/ship.rb
+++ lib/space_school/ship.rb
@@ -2,6 +2,10 @@ class Ship
   attr_reader :current_position
 
   def initialize
-    @current_position = [5,0] # x, y
+    @current_position = [5, 0] # x, y
+  end
+
+  def right
+    @current_position[0] += 1
   end
 end
